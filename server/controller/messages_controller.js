21223bb71e42410582efbc11a13e10b7b9d6e141
Brandon Warner <warner.brandon.a@gmail.com>
2020-01-27 15:03:02 -0600

    Remove custom timestamp function

--- server/controller/messages_controller.js
+++ server/controller/messages_controller.js
@@ -1,48 +1,47 @@
 var messages = [];
 var id = 0;
 
-var getTimeStamp = () => {
-    var date = new Date();
-    let month, day, year, hours, minutes, seconds;
+// var getTimeStamp = () => {
+//     var date = new Date();
+//     let month, day, year, hours, minutes, seconds;
 
-    month = date.getMonth();
-    day = date.getDate();
-    year = date.getFullYear();
+//     month = date.getMonth();
+//     day = date.getDate();
+//     year = date.getFullYear();
 
-    hours = date.getHours();
-    minutes = date.getMinutes();
-    seconds = date.getSeconds();
+//     hours = date.getHours();
+//     minutes = date.getMinutes();
+//     seconds = date.getSeconds();
     
-    let dateArray = [(month + 1), day];
-    let formattedDate = dateArray.map(
-        val => {
-            if(val < 10){
-                return '0' + val;
-            }
-            return val;
-        }
-    );
+//     let dateArray = [(month + 1), day];
+//     let formattedDate = dateArray.map(
+//         val => {
+//             if(val < 10){
+//                 return '0' + val;
+//             }
+//             return val;
+//         }
+//     );
 
-    let timeArray = [hours, minutes, seconds];
-    let formattedTime = timeArray.map(
-        (val) => {
-            if(val < 10){
-                return '0' + val;
-            }
-            else{
-                return val;
-            }
-        }
-    );
+//     let timeArray = [hours, minutes, seconds];
+//     let formattedTime = timeArray.map(
+//         (val) => {
+//             if(val < 10){
+//                 return '0' + val;
+//             }
+//             else{
+//                 return val;
+//             }
+//         }
+//     );
 
-    return `${formattedDate.join('/')}/${year} - ${formattedTime.join(':')}`;
+//     return `${formattedDate.join('/')}/${year} - ${formattedTime.join(':')}`;
 
-}
+// }
 
 
 var createMessage = (req, res) => {
-    let {text} = req.body;
-    let time = getTimeStamp();
+    let {text, time} = req.body;
 
     var newMessage = {
         id: id,
